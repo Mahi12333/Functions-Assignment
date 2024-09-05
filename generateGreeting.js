7c2e2d847b79e4edb9e017787ae0be1cc60cf78a
Mahitosh Giri <88604119+Mahi12333@users.noreply.github.com>
2024-09-05 23:10:12 +0530

    Create generateGreeting.js

---
+++ generateGreeting.js
@@ -0,0 +1,11 @@
+
+const generateGreeting = function (name){
+    return `Heloo good morning ${name}`
+}
+
+let user1= generateGreeting("Rahul")
+console.log(user1);
+let user2= generateGreeting("Ramesh")
+console.log(user2);
+let user3= generateGreeting("Ram")
+console.log(user3);
